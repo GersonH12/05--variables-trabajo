a0280909e016840e72c12dd87c28673571a2579a
¨Gerson¨ <gh0905869@gmail.com>
2024-11-29 17:14:19 -0500

    noveno commit

---
+++ js/ejercicios9.js
@@ -0,0 +1,14 @@
+// Películas o series para ver 
+// Define un array miListaDePendientes con títulos de cuatro películas o series que quieres 
+// ver. 
+// • Usa el método push para agregar una quitan película.  
+// • Modifica el nombre de la 2da película del array por una nueva. 
+// • Muestra el array actualizado por consola. 
+
+let miListaDePendientes = ["Juego de Tronos", "Avengers", "Thor", "Avatar"]
+
+miListaDePendientes.push("Interstellar");
+miListaDePendientes[1] = "Transformers";
+
+console.log("Mi lista actualizadas de peliculas:");
+console.log(miListaDePendientes);
