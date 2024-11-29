76a02b411305b64f297ad40659534f397b653a8f
¨Gerson¨ <gh0905869@gmail.com>
2024-11-29 16:35:06 -0500

    Tercer commit

---
+++ js/ejercicios3.js
@@ -0,0 +1,9 @@
+// 3. Solicita al usuario dos números y una operación (+, -, *, /) usando prompt. Muestra el 
+// resultado de la operación en un alert. 
+
+let num1 = parseFloat(prompt("Ingrese el primer numero:"));
+let num2 = parseFloat(prompt("Ingrese el segundo numero"));
+
+let result = num1 + num2;
+
+alert (`El resultado de la suma es: ${result}`);
