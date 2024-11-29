252455ce742df982520e3d7da26b78e475e67440
¨Gerson¨ <gh0905869@gmail.com>
2024-11-29 16:25:31 -0500

    segundo commit

---
+++ js/ejercicios2.js
@@ -0,0 +1,11 @@
+// 2. Crea variables para tu color favorito, comida favorita y lugar favorito (todas string). Usa 
+// template literals para formar una frase como: 
+// `Mi color favorito es [color], me encanta comer [comida] y quiero visitar [lugar].`
+
+const colorFavorito = "verde";
+const comidaFavorita = "arrozConPollo";
+const lugarFavorito = "Alemania";
+
+const frase = `Mi color favorito es ${colorFavorito}, mi comida favorita es ${comidaFavorita}, y mi lugar favorito es ${lugarFavorito}.`;
+
+console.log(frase);
