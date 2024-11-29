b22e76a53da0a18362d22e2660318249b3cc9f2a
¨Gerson¨ <gh0905869@gmail.com>
2024-11-29 16:09:28 -0500

    Primer commmit

---
+++ js/ejercicios1.js
@@ -0,0 +1,9 @@
+// 1. Calcular el área de un rectángulo: Pide al usuario la base y la altura de un rectángulo, y 
+// muestra el área en la consola.
+
+const base = parseFloat(prompt("Ingerese la base dedl rectangulo"));
+const altura = parseFloat(prompt("Ingrese la altura del rectangulo"));
+
+const area = base * altura;
+
+console.log(`el area es:${area}`);
